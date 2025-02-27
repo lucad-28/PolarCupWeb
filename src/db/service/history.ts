0d712597faa2702cce8c369756a15748fe02bd8e
lucad-28 <lucadev.rll28@gmail.com>
2025-02-26 20:07:47 -0500

    Update API_URL

--- src/db/service/history.ts
+++ src/db/service/history.ts
@@ -1,14 +1,15 @@
 import { History } from "@/types/history";
+import { _API_URL } from "@/lib/utils";
 
 export class HistoryService {
   async getAll(): Promise<History[]> {
-    const res = await fetch("http://localhost:5000/api/history/");
+    const res = await fetch(`${_API_URL}/history/`);
     const data = await res.json();
     return data as History[];
   }
 
   async getResampled(): Promise<History[]> {
-    const res = await fetch("http://localhost:5000/api/history/resampled");
+    const res = await fetch(`${_API_URL}/history/resampled`);
     const data = await res.json();
     return data as History[];
   }
